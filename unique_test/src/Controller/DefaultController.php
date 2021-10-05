631e9903114202f24da298e5d0483339b48c02a0
ced <cedarg213@gmail.com>
2021-10-05 08:47:43 +0200

    Etape 1 : Done !

--- unique_test/src/Controller/DefaultController.php
+++ unique_test/src/Controller/DefaultController.php
@@ -14,24 +14,26 @@ class DefaultController extends AbstractController
 {
     public function index(): Response
     {
-        $data = $this->getParameter('app.shop_name');
-        $api_key = $this->getParameter('app.shop_api_key');
-        $api_secret = $this->getParameter('app.shop_api_secret');
-        $api_pwd = "";
-        $shop = $this->getParameter('app.shop_name');
-        $api_token = $this->getParameter('app.shop_api_token');
+        // ************************************************
+        // 1) se connecter aux APIs de la boutique Shopify
+            $data = $this->getParameter('app.shop_name');
+            $api_key = $this->getParameter('app.shop_api_key');
+            $api_secret = $this->getParameter('app.shop_api_secret');
+            $shop = $this->getParameter('app.shop_name');
+            $api_token = $this->getParameter('app.shop_api_token');
 
-        // Create options for the API
-        $options = new Options();
-        $options->setVersion('2020-01');
+            // Create options for the API
+            $options = new Options();
+            $options->setVersion('2020-01');
 
-        // Create the client and session
-        $api = new BasicShopifyAPI($options);
-        $api->setSession(new Session($shop, $api_token));
+            // Create the client and session
+            $api = new BasicShopifyAPI($options);
+            $api->setSession(new Session($shop, $api_token));
+        // ETAPE 1 : DONE !!!
+        // ************************************************
 
-
-        $response = $api->rest('GET', '/admin/products.json', ['limit' => 5]);
-        $products = $response['body']['container']['products'];
+            $response = $api->rest('GET', '/admin/products.json', ['limit' => 5]);
+            $products = $response['body']['container']['products'];
 
 
         return $this->render('testUniq/welcometest.html.twig', [
